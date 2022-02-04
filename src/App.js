0656edd8969a086757f96c5667eb1cfbb8d54535
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2022-02-04 09:45:21 +0600

    simple todo applicaton  with localstorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [update, setUpdate] = useState(false);
+  const [id, setId] = useState(null);
+  useEffect(() => {
+    const todo = window.localStorage.getItem("todo");
+    setTasks(JSON.parse(todo));
+    console.log(todo);
+  }, []);
+
+  const taskHandeler = (e) => {
+    e.preventDefault();
+    if (!title) {
+      return;
+    }
+    const newData = {
+      id: new Date().getTime() + Math.random(),
+      title: title,
+    };
+    setTasks([newData, ...tasks]);
+    window.localStorage.setItem("todo", JSON.stringify([newData, ...tasks]));
+    setTitle("");
+  };
+
+  const editHandeler = (id) => {
+    const editTask = tasks.find((task) => task.id === id);
+    setTitle(editTask.title);
+    if (id === editTask.id) {
+      setUpdate(true);
+      setId(editTask.id);
+    } else {
+      setUpdate(false);
+      setId(null);
+    }
+  };
+  const updateHandeler = (e) => {
+    e.preventDefault();
+    const updateTask = tasks.map((task) =>
+      task.id === id ? { ...task, id: id, title: title } : task
+    );
+    setTasks(updateTask);
+    window.localStorage.setItem("todo", JSON.stringify(updateTask));
+    console.log(updateTask);
+    setUpdate(false);
+    setTitle("");
+    // console.log(newData);
+  };
+  const deleteHandeler = (id) => {
+    const deleteTask = tasks.filter((task) => task.id !== id);
+    window.localStorage.setItem("todo", JSON.stringify(deleteTask));
+    setTasks(deleteTask);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={update ? updateHandeler : taskHandeler}>
+        <label htmlFor="title">Title</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          placeholder="type your title"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <button type="sumbit">{update ? "update" : "sumbit"}</button>
+      </form>
+      <div>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {task.title}{" "}
+              <button onClick={() => editHandeler(task.id)}>edit</button>
+              <button onClick={() => deleteHandeler(task.id)}>delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
